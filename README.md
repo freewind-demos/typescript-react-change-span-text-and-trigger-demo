75b2a082219fad4f91a90d74e5b7c5449365dfa2
Freewind <nowindlee@gmail.com>
2020-02-12 18:57:44 +0800

    typescript-react-change-input-value-and-trigger-demo

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-TypeScript React Hello World Demo
+TypeScript React Change Input Value and Trigger Demo
 =================================
+
+从react之外修改某个input的值，并且触发相应的事件，让react感知到这个变化。
+
+由于react修改了`input.value`的相关方法，直接调用`input.value=?`无效，
+需要使用特别办法。
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,29 @@
-//import * as React from 'react'
-import React from 'react'
+import React, {useState} from 'react'
+
+function changeFromOutside() {
+  console.log("> changeFromOutside");
+  const myInput = document.querySelector<HTMLInputElement>('#myInput')!;
+
+  // Notice: only input value changes, but not trigger state change
+  // myInput.value = 'new value';
+  // myInput.dispatchEvent(new Event('input', {bubbles: true}));
+
+  const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value")?.set;
+  nativeInputValueSetter?.call(myInput, 'new value');
+
+  // `input` event is also ok
+  myInput.dispatchEvent(new Event('change', {bubbles: true}));
+}
 
 export default function Hello() {
+  const [message, setMessage] = useState('react')
   return <div>
-    <h1>Hello React</h1>
+    <h1>Hello {message}</h1>
+    <div>
+      <input id="myInput" value={message} onChange={(event) => setMessage(event.target.value)}/>
+    </div>
+    <div>
+      <button onClick={() => changeFromOutside()}>Change from outside</button>
+    </div>
   </div>
 };
